31b9f64c3450ef3805e8c45168fa7db5ea87f955
marcvickers12-svg <marcvickers12@gmail.com>
2025-09-27 16:52:38 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,10 @@
+
+"""
+DGA Package Initializer
+-----------------------
+This file marks the `dga` folder as a Python package so that
+`from dga.something import ...` works correctly.
+"""
+
+# Optional: expose submodules for convenience
+from . import utils, database, auth_utils, analysis
